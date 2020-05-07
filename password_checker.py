fb3398e9b9b631a00fd2592873c62ae69dd9cf97
Carl Padilla <carlpadilla@protonmail.com>
2020-05-06 22:35:41 -0400

    first commit

---
+++ password_checker.py
@@ -0,0 +1,6 @@
+# password checker
+username = input('Enter username ')
+password = input('Enter password ')
+
+
+print(f'{username}, your password {len(password) * "*"} is {len(password)} characters long')
