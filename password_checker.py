e0136bbf5d8cff0c5fbb24227619746bd0c30936
Carl Padilla <carlpadilla@protonmail.com>
2020-05-06 22:41:09 -0400

    refactored code

--- password_checker.py
+++ password_checker.py
@@ -1,6 +1,9 @@
-# password checker
+# password checker - take in a username and password and outputs the username and password length
 username = input('Enter username ')
 password = input('Enter password ')
 
+password_length = len(password)
+hidden_password = "*" * password_length
 
-print(f'{username}, your password {len(password) * "*"} is {len(password)} characters long')
+
+print(f'{username}, your password {hidden_password} is {password_length} characters long')
