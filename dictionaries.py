a473382fb19e3577d677aa524bc0cc03b3e68e4c
Carl Padilla <carlpadilla@protonmail.com>
2020-05-17 12:29:24 -0400

    added dictionary examples

---
+++ dictionaries.py
@@ -0,0 +1,25 @@
+# Dictionary - is an unordered key value pair
+
+dictionary = {
+    'a': [1, 2, 3],
+    'b': 'hello',
+    'x': True
+}
+
+my_list = [
+    {
+        'a': [1, 2, 3],
+        'b': 'hello',
+        'x': True
+    },
+    {
+        'a': [4, 5, 6],
+        'b': 'world',
+        'x': False
+    }
+
+]
+
+print(dictionary)
+print(dictionary['b'])
+print(my_list[1]['a'][2])
