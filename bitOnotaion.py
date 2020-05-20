cbbc19b0ad3057f220b04a775c87aa59d3ce576e
carlpadilla <carlpadilla@protonmail.com>
2020-05-19 22:00:01 -0400

    saving bigo

---
+++ bitOnotaion.py
@@ -0,0 +1,30 @@
+
+
+def find_name(name, phone_book):
+
+    for person in phone_book:
+        if name == person:
+            return True
+
+
+name = 'Carl'
+phone_book = ['Milani', 'Jenny', 'Nina', 'boy,', 'Carl']
+
+O(n)
+
+
+def find_name(austin_phone_book, philly_phone_book):
+    for austinite in austin_phone_book:
+        for philadelphian in philly_phone_book:
+            if austinite == philadelphian
+            return True
+
+
+austin_phone_book = ['Milani', 'Jenny', 'Nina', 'boy,', 'Carl']
+Philly_phone_book = ['Melida', 'Marco', 'Rosa', 'Carl,', 'Jimmy']
+
+O(n ^ 2)
+O(16)
+
+O(6 + 1 + 36)
+O(n + 1 + n ^ 2)
