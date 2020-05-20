8d1555ce395983d845b37489ab5be76ff6dca214
carlpadilla <carlpadilla@protonmail.com>
2020-05-20 01:04:12 -0400

    saving ternary

---
+++ ternary.py
@@ -0,0 +1,8 @@
+# Ternary Operator
+
+# condition_if_true if condition else condition_false
+
+is_friend = True
+can_message = 'Message allowed' if is_friend else "not allowed to message"
+
+print(can_message)
