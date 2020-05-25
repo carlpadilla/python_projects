2f069d89caff62b4df52444f46eb6bdd3f5b8ea9
Carl Padilla <carlpadilla@protonmail.com>
2020-05-25 19:59:05 -0400

    added highest_even"

--- mini_programs/pounds_to_kilos.py
+++ mini_programs/pounds_to_kilos.py
@@ -1,3 +1,10 @@
-weight = input('Enter your weight in pounds: ')
-kilos = int(weight) * 0.45
-print(f'Your weight in pounds converts to {kilos}kg')
+# weight = input('Enter your weight in pounds: ')
+# kilos = int(weight) * 0.45
+# print(f'Your weight in pounds converts to {kilos}kg')
+
+fruits = ["apple", "pear", "orange", "mango", "peach"]
+
+print(fruits[0])     # 1
+print(fruits[-5])    # 2
+# print(fruits[5])     # 3
+print(fruits[4-2])   # 4
