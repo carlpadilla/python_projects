c30376bb112ed661d15b0a4acd2815e838e87889
Carl Padilla <carlpadilla@protonmail.com>
2020-05-28 03:51:49 -0400

    added quadratic time example

--- BigO/bigoexamples.py
+++ BigO/bigoexamples.py
@@ -27,3 +27,29 @@ def func_lin(values):
 
 print("Linear Time")
 func_lin(lst)
+
+"""
+O(n^2) - Quadratic Time
+
+Note how we now have two loops, one nested inside another. This means that for a list of n items, we will have to 
+perform n operations for every item in the list! This means in total, we will perform n times n assignments, 
+or n^2. So a list of 10 items will have 10^2, or 100 operations. You can see how dangerous this can get for very 
+large inputs! 
+
+"""
+
+
+def func_quad(lst):
+    """
+    Prints pairs for every item in the list.
+    :param lst:
+    :return:
+    """
+
+    for item_1 in lst:
+        for item_2 in lst:
+            print(item_1, item_2)
+
+
+print('Quadratic Time')
+func_quad(lst)
