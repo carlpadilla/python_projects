7261c35f1655ae8b282d092fdef475e0c335a491
Carl Padilla <carlpadilla@protonmail.com>
2020-07-01 10:14:42 -0400

    added reduce example

--- functions/map.py
+++ functions/map.py
@@ -1,4 +1,5 @@
-# map, filter, zip and reduce
+# map, filter, and reduce
+from functools import reduce
 
 """ map() function returns a map object(which is an iterator) of the results after applying the given function to each item of a given iterable (list, tuple etc.)"""
 
@@ -27,3 +28,16 @@ def only_odd(item):
 
 print(list(filter(only_odd, my_list)))
 print(my_list)
+
+
+# Reduce
+"""The reduce(fun,seq) function is used to apply a particular function passed in its argument to all of the list elements mentioned in the sequence passed along.This function is defined in “functools” module.
+"""
+
+
+def accumulator(acc, item):
+    print(acc, item)
+    return acc + item
+
+
+print(reduce(accumulator, my_list, 0))
