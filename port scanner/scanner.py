84352348026ad1ba7762911531c6fdca9b3224ff
Carl Padilla <carlpadilla@protonmail.com>
2020-07-06 10:13:52 -0400

    scanning all ports

--- port scanner/scanner.py
+++ port scanner/scanner.py
@@ -22,10 +22,11 @@ print("_" * 50)
 
 
 try:
-    for port in range(50, 85):
+    for port in range(1, 65535):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(1)
         result = s.connect_ex((target, port))  # returns an error indicator
+        # print('Checking port {}'.format(port))
         if result == 0:
             print("Port {} is open".format(port))
         s.close()
