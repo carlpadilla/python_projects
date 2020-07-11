5fb181e6e049ea69775ea68d50adbb7121149b6c
Carl Padilla <carlpadilla@protonmail.com>
2020-07-11 17:48:20 -0400

    completed challenege

--- functions/challenge.py
+++ functions/challenge.py
@@ -1,16 +1,17 @@
 from functools import reduce
 
+print('#1')
 # 1 Capitalize all of the pet names and print the list
 my_pets = ['sisi', 'bibi', 'titi', 'carla']
 
 
 def uppercase(names):
-    return str(names.capitalize())
+    return str(names.upper())
 
 
 print(list(map(uppercase, my_pets)))
 
-
+print('#2')
 # 2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
 my_strings = ['a', 'b', 'c', 'd', 'e']
 my_numbers = [5, 4, 3, 2, 1]
@@ -18,6 +19,8 @@ my_numbers = [5, 4, 3, 2, 1]
 sorted_num = sorted(my_numbers)
 
 print(list(zip(my_strings, sorted_num)))
+
+print('#3')
 
 # 3 Filter the scores that pass over 50%
 scores = [73, 20, 65, 19, 76, 100, 88]
@@ -29,5 +32,14 @@ def check_if_over_50(i):
 
 print(list(filter(check_if_over_50, scores)))
 
+print('#4')
 
 # 4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
+
+
+def accumulator(acc, item):
+    # print(acc, item)
+    return acc + item
+
+
+print(reduce(accumulator, (my_numbers + scores), 0))
